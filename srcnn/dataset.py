5f91cf84669cf3b35ec34491a86dd2386380e8f2
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2019-09-26 22:22:01 +0300

    extract util functions into separate files

---
+++ srcnn/dataset.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+
+
+def resize(image, size, method):
+    resized = tf.image.resize(image, size, method)
+    if resized.dtype == tf.uint8:
+        return resized
+    else:
+        # deal with https://github.com/tensorflow/tensorflow/issues/10722
+        return tf.saturate_cast(resized, tf.uint8)
+
+
+def prepare_input_image(original_image, input_h, input_w, scale):
+    degraded_image = resize(original_image, size=[input_h // scale, input_w // scale],
+                            method=tf.image.ResizeMethod.GAUSSIAN)
+    input_image = resize(degraded_image, size=[input_h, input_w], method=tf.image.ResizeMethod.BICUBIC)
+    return tf.image.convert_image_dtype(input_image, dtype=tf.float32)
+
+
+def prepare_output_image(original_image, input_h, input_w, output_h, output_w):
+    if output_h == input_h & output_w == input_w:
+        output = original_image
+    else:
+        new_x = (input_w - output_w) // 2
+        new_y = (input_h - output_h) // 2
+        output = tf.image.crop_to_bounding_box(original_image, new_y, new_x, output_h, output_w)
+    return tf.image.convert_image_dtype(output, dtype=tf.float32)
+
+
+def prepare_crops(filename, input_size, stride, scale, padding=None):
+    image_string = tf.io.read_file(filename)
+    image = tf.image.decode_bmp(image_string)
+    if padding is not None:
+        image = tf.pad(image, tf.contsant([[padding, padding], [padding, padding], [0, 0]]), "CONSTANT")
+    crops = tf.image.extract_patches(tf.expand_dims(image, 0), [1, input_size, input_size, 1],
+                                     [1, stride, stride, 1], [1, 1, 1, 1], 'VALID')
+    return tf.reshape(crops[0], [-1, input_size, input_size, 3])
+
+
+def parse_function(filename, input_size, output_size, stride, scale):
+    crops = prepare_crops(filename, input_size, stride, scale)
+    input_dataset = tf.data.Dataset.from_tensors(crops).map(
+        lambda img: prepare_input_image(img, input_size, input_size, scale))
+    output_dataset = tf.data.Dataset.from_tensors(crops).map(
+        lambda img: prepare_output_image(img, input_size, input_size, output_size, output_size))
+    result = tf.data.Dataset.zip((input_dataset, output_dataset))
+    return result
---
+++ srcnn/plot.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+def plot_image(image, label=''):
+    plt.imshow(image)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel(label)
+
+
+def plot_images(images, grid_size):
+    plt.figure(figsize=(8, 8))
+    for n, image in enumerate(images[0:grid_size ** 2]):
+        plt.subplot(grid_size, grid_size, n + 1)
+        plot_image(image, n)
+
+
+def plot_dataset(d, n_patches=8):
+    plt.figure(figsize=(2 * n_patches, 8))
+    for n, images in enumerate(d.take(n_patches)):
+        input_image = images[0]
+        output_image = images[1]
+        ih, iw, _ = input_image.get_shape()
+        oh, ow, _ = output_image.get_shape()
+
+        padding = (ih - oh) // 2
+        padded_output = tf.pad(output_image, [[padding, padding], [padding, padding], [0, 0]], "CONSTANT")
+
+        plt.subplot(n_patches, 2, 2 * n + 1)
+        plot_image(tf.image.convert_image_dtype(input_image, dtype=tf.uint8), str(n))
+        plt.subplot(n_patches, 2, 2 * n + 2)
+        plot_image(tf.image.convert_image_dtype(padded_output, dtype=tf.uint8), str(n))
+
+
+def plot_predictions(model, names, inputs, ground_truth):
+    inputs = tf.stack(inputs)
+    ground_truth = tf.stack(ground_truth)
+    outputs = tf.clip_by_value(model.predict(inputs), 0, 1)
+
+    _, in_h, in_w, _ = inputs.get_shape()
+    _, out_h, out_w, _ = outputs.shape
+    new_x = (in_w - out_w) // 2
+    new_y = (in_h - out_h) // 2
+    inputs = tf.image.crop_to_bounding_box(inputs, new_y, new_x, out_h, out_w)
+
+    for i, o, g in zip(inputs, outputs, ground_truth):
+        plt.figure(figsize=(12, 12))
+        plt.subplot(1, 3, 1)
+        plot_image(i, "input")
+        plt.subplot(1, 3, 2)
+        plot_image(o, "output")
+        plt.subplot(1, 3, 3)
+        plot_image(g, "ground truth")
+
+    psnr_inputs = tf.image.psnr(inputs, ground_truth, max_val=1.0)
+    psnr_outputs = tf.image.psnr(outputs, ground_truth, max_val=1.0)
+    psnr = []
+    for name, psnr_i, psnr_o in zip(names, psnr_inputs, psnr_outputs):
+        psnr.append([name, f"{psnr_i:.3f}", f"{psnr_o:.3f}"])
+
+    return psnr
